e10915736aad7414cfa447f0e6febc8df7243195
Arthur <arthursantos48211@gmail.com>
2023-07-06 00:47:26 -0300

    Arquivos iniciais

---
+++ components/scripts/modal.ts
@@ -0,0 +1,42 @@
+// Arquivo que contém as funções principais do projeto
+/** 
+ * Função que realiza a requisição para a API 
+ * e retorna um novo meme gerado
+*/
+export function generateMeme (): void {
+    fetch(
+        'https://meme-api.com/gimme'
+    ).then(
+        (res: Response) => res.json().then(
+            (data: Record<string, string>) => updateDetails(data.url, data.title, data.author)
+        )
+    ); 
+}
+
+/** Função que atualiza a página com os dados recebidos da API
+ * 
+ * @param url url da imagem do meme
+ * 
+ * @param title Título do meme/postagem
+ * 
+ * @param author Nome do autor do meme
+*/
+export function updateDetails (url: string, title: string, author: string): void {
+    const meme_img: HTMLImageElement = document.querySelector('.meme-generator > img') as HTMLImageElement;
+    const meme_title: HTMLHeadingElement = document.querySelector('.meme-title') as HTMLHeadingElement;    
+    const meme_author: HTMLDivElement = document.querySelector('.meme-author') as HTMLDivElement;
+
+    meme_img.setAttribute('src', url);
+    
+    meme_title.innerHTML = title;
+ 
+    meme_author.innerHTML = author;
+}
+
+/** 
+ * Função disparada quando
+ * o usuário sai da página do projeto
+ * 
+ * @returns { string } Altera o título da página
+ */
+export const windowBlur = (): string => document.title = 'Go back :(';
---
+++ components/scripts/script.ts
@@ -0,0 +1,13 @@
+import { generateMeme, windowBlur } from "./modal";
+
+window.onload = () => {
+    const generate_btn: HTMLButtonElement = document.querySelector('.generate-btn') as HTMLButtonElement;
+    
+    const meme_img: HTMLImageElement = document.querySelector('img') as HTMLImageElement;
+
+    meme_img.setAttribute('src', '../imgs/lupa.png');
+
+    generate_btn.addEventListener('click', generateMeme);
+
+    window.addEventListener('blur', windowBlur);
+}
